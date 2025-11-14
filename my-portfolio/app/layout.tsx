aea997054447ad7c5509dc877fa36f861c4bee6e
Wilfred Osozi <osoziw@gmail.com>
2025-11-15 00:57:01 +0300

    updated the header

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "@/components/common/Footer";
 import "./globals.css";
+import Header from "@/components/common/Header";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,6 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <title>Wilfred Osozi Portfolio</title>
       </head>
+      <Header/>
       <body className="min-h-screen flex flex-col">
         <main className="flex-1">
           {children}
